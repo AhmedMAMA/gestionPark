1f5c2ab60ced010559e1b7eca107328912e9e3bd
ahmed <ahmed.mama@efrei.net>
2025-03-13 02:10:24 +0100

    Réorganisation Correct des fichiers dont le mieux est celui du dossier main, fichier test.py ajouté

---
+++ main/test.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# #End Require
+
+from classes.StructureCLasses import main
+
+racine_auchan = "/home/ahmed/Bureau/Projet/OpenCV/images/auchan.webp"
+racine_carrefour_jpeg = "/home/ahmed/Bureau/Projet/OpenCV/images/carrefour.jpeg"
+racine_carrefour_webp = "/home/ahmed/Bureau/Projet/OpenCV/images/carrefour.webp"
+
+print("ESSAI AVEC L'IMAGE AUCHAN.WEBP")
+main(racine_auchan)
+
+print("ESSAI AVEC L'IMAGE CARREFOUR.JPEG")
+main(racine_carrefour_jpeg)
+
+print("ESSAI AVEC L'IMAGE CARREFOUR/WEBP")
+main(racine_carrefour_webp)
